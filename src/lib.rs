fc28a3f30bf8116f80e060dad6a4bb34a90910ec
GMargherini <giorgio.margherini@gmail.com>
2025-06-15 16:57:49 +0100

    implemented keyboard events

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,22 @@ pub mod core;
 pub mod deck;
 pub mod pile;
 
+const HELP_SCREEN: &'static str = "Controls:\n
+    A                   Automatically finish the game if all cards are uncovered\n
+    ?                   Print help\n
+    Q | Esc             Quit game\n
+    L                   Print move history\n
+    [Pile1][Pile2]      Automatically move cards from Pile1 to Pile2\n
+    N                   Draw a card from the uncovered pile\n
+    U | Backspace       Undo last move\n
+    Pile can be any among 1-7, P, C, D, H, S";
+
 pub fn setup() -> Game {
     Game::new()
 }
 
 pub fn print_table(table: &Table) {
-    println!(" D\t P\t\t C\u{2663}\t D\u{2666}\t H\u{2665}\t S\u{2660}");
+    println!(" N\t P\t\t C\u{2663}\t D\u{2666}\t H\u{2665}\t S\u{2660}");
     print!("{}\t{}\t\t", table.draw_pile(), table.uncovered_pile());
     let suit_piles = deck::card::Suit::iter().map(|suit| table.suit_pile(suit));
     suit_piles.for_each(|suit_pile| print!("{}\t", suit_pile));
@@ -41,16 +51,5 @@ pub fn print_table(table: &Table) {
 }
 
 pub fn print_help() {
-    println!(
-        "Controls:\n
-    A                   Automatically finish the game if all cards are uncovered\n
-    H                   Print help\n
-    Q                   Quit game\n
-    L                   Print move history\n
-    [Pile1][Pile2]      Automatically move cards from Pile1 to Pile2\n
-    [Pile1][Pile2][n]   Move n cards from Pile1 to Pile2\n
-    D                   Draw a card from the uncovered pile\n
-    U                   Undo last move\n
-    Pile can be any between 1-7, P, C, D, H, S"
-    );
+    println!("{}", HELP_SCREEN);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::io::{Result, Write, stdin, stdout};
-
 use solitaire::{
     self,
     core::{Error, moves::Move},
 };
+use std::io::{Result, Write, stdin, stdout};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
 fn main() -> Result<()> {
     let mut game = solitaire::setup();
     while !game.is_over() {
         clear_screen();
-        println!("{game}");
+        println!("\n{game}");
         solitaire::print_table(game.table());
-        let input = take_input("\n>")?;
+        let input = take_input();
         let next_move = Move::from(input);
         if let Err(e) = game.play(next_move) {
             match e.downcast::<Error>() {
@@ -22,15 +23,19 @@ fn main() -> Result<()> {
                     Error::Help => {
                         clear_screen();
                         solitaire::print_help();
+                        println!("Press Enter to continue");
                     }
                     Error::History => {
                         clear_screen();
                         game.print_history();
+                        println!("Press Enter to continue");
                     }
                     Error::Win => break,
                     _ => {
+                        let mut stdout = stdout().into_raw_mode().unwrap();
                         let err = ansi_term::Colour::Red.paint(err.to_string());
-                        eprintln!("{}", err);
+                        write!(stdout, "{}{}", termion::cursor::Goto(1, 1), err).unwrap();
+                        stdout.flush().unwrap();
                     }
                 },
                 Err(err) => {
@@ -38,7 +43,7 @@ fn main() -> Result<()> {
                     eprintln!("{}", err);
                 }
             }
-            take_input("")?;
+            let _ = take_input();
         }
     }
     clear_screen();
@@ -47,15 +52,49 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn take_input(hint: &str) -> Result<String> {
-    let mut buffer = String::new();
-    print!("{hint} ");
-    let _ = stdout().flush();
+fn take_input() -> String {
     let stdin = stdin();
-    stdin.read_line(&mut buffer)?;
-    Ok(buffer)
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut input = String::new();
+    for (i, k) in stdin.keys().enumerate() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(i as u16 + 1, 1),
+            termion::cursor::Hide
+        )
+        .unwrap();
+        let s = match k.as_ref().unwrap() {
+            Key::Char('\n') => break,
+            Key::Char('l') => return String::from("l"),
+            Key::Char('?') => return String::from("?"),
+            Key::Char('n') => return String::from("n"),
+            Key::Char('a') => return String::from("a"),
+            Key::Backspace | Key::Char('u') => return String::from("u"),
+            Key::Esc | Key::Char('q') => return String::from("q"),
+            Key::Char(c) => {
+                println!("{c}");
+                *c
+            }
+            _ => return String::from("h"),
+        };
+        let s = String::from(s);
+        input.push_str(&s[..]);
+        if input.len() == 2 {
+            return input;
+        }
+    }
+    input
 }
 
 fn clear_screen() {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(
+        stdout,
+        "{}{}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1)
+    )
+    .unwrap();
+    stdout.flush().unwrap();
 }
