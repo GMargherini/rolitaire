ad1322052e393029e491130d2233acde4fc1ddd9
GMarghreini <giorgio.margherini@gmail.com>
2026-01-24 21:55:14 +0100

    simplified flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          packages = with pkgs; [
-            rust-analyzer
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+        ];
+        packages = with pkgs; [
+          rust-analyzer
+          rustfmt
+          rustPackages.clippy
+        ];
+      };
+    };
 }
