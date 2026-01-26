c6dce8c34e73bb8e5a237fe5eb0f78e94a8d17ce
GMargherini <giorgio.margherini@gmail.com>
2026-01-26 13:57:30 +0100

    generalized flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,31 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          rustc
-        ];
-        packages = with pkgs; [
-          rust-analyzer
-          rustfmt
-          rustPackages.clippy
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            cargo
+            rustc
+          ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
+        devShells.${system}.default = pkgs.mkShell
+        {
+          packages = with pkgs; [
+            rust-analyzer
+            rustfmt
+            pre-commit
+            rustPackages.clippy
+          ];
+        };
+      }
+    );
 }
